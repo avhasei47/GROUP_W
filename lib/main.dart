88f21ef30843e7e43a397cecd862adc0f71772d3
lesegomochai23 <lesegomochai23@gmail.com>
2026-05-12 12:56:43 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+// ============================================
+// TPG316C Group Assignment – Student Assistant App
+// Group: GROUP_W
+// Members:
+// 1. Ramabulana Avhasei – 221007752
+// 2. Jokazi Nothabile –  223060076
+// 3. Lesego Mochai –  222046558
+// 4. Mdolo Kwanele – 223088602 
+// 5. Mchunu Precious – 222078878
+// File: main.dart
+// Description: Entry point of the application. Initializes Supabase,
+//              sets up MVVM providers, and configures routing.
+// ============================================
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,6 +22,7 @@ import 'viewmodels/auth_viewmodel.dart';
 import 'viewmodels/application_viewmodel.dart';
 import 'viewmodels/admin_viewmodel.dart';
 
+// The application's entry point.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -17,13 +31,16 @@ void main() async {
   runApp(const StudentAssistantApp());
 }
 
+// The root widget of the application
 class StudentAssistantApp extends StatelessWidget {
   const StudentAssistantApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
+      // Registers all ViewModels with Provider so they can be accessed anywhere
       providers: [
+        // Manages authentication state
         ChangeNotifierProvider<AuthViewModel>(create: (_) => AuthViewModel()),
 
         ChangeNotifierProvider<ApplicationViewModel>(
@@ -33,10 +50,14 @@ class StudentAssistantApp extends StatelessWidget {
         ChangeNotifierProvider<AdminViewModel>(create: (_) => AdminViewModel()),
       ],
       child: MaterialApp(
+        // Removes the "DEBUG" banner in the top‑right corner
         debugShowCheckedModeBanner: false,
         title: 'Student Assistant Application System',
+        // Applies the custom light theme (defined in AppTheme)
         theme: AppTheme.lightTheme,
+        // The first screen shown when the app launches (login screen).
         initialRoute: RouteManager.login,
+        // Uses named routes defined in RouteManager to handle navigation
         onGenerateRoute: RouteManager.generateRoute,
       ),
     );
