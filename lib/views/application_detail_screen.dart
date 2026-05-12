e5041bcbbb52d5be2f3edd327e7e08abe3403858
preciousmchunu423 <preciousmchunu423@gmail.com>
2026-05-12 09:05:03 +0200

    Update application_detail_screen.dart

--- lib/views/application_detail_screen.dart
+++ lib/views/application_detail_screen.dart
@@ -1,3 +1,16 @@
+// ============================================
+// TPG316C Group Assignment – Student Assistant App
+// Group: GROUP_W
+// Members:
+// 1. Ramabulana Avhasei – 221007752
+// 2. Jokazi Nothabile –  223060076
+// 3. Lesego Mochai –  222046558
+// 4.  Mdolo Kwanele – 223088602 
+// 5.  Mchunu Precious  – 222078878
+// File: application_detail_screen.dart
+// Description: Displays full application details. Allows edit/delete only when status is Pending.
+// ============================================
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
