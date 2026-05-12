da6754033ec23c97f8feff0cb17d9e2c6aecb7b4
lesegomochai23 <lesegomochai23@gmail.com>
2026-05-12 12:51:01 +0200

    Update appilication_service.dart

--- lib/services/appilication_service.dart
+++ lib/services/appilication_service.dart
@@ -1,6 +1,20 @@
+// ============================================
+// TPG316C Group Assignment – Student Assistant App
+// Group: GROUP_W
+// Members:
+// 1. Ramabulana Avhasei – 221007752
+// 2. Jokazi Nothabile –  223060076
+// 3. Lesego Mochai –  222046558
+// 4. Mdolo Kwanele – 223088602 
+// 5. Mchunu Precious – 222078878
+// File: application_service.dart
+// Description: Service class for Supabase CRUD operations on student applications.
+//              Handles fetching, creating, updating, and deleting applications.
+// ============================================
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 
+// Service that manages all Supabase interactions for Student Assistant applications.
 class AuthViewModel extends ChangeNotifier {
   final AuthService _authService = AuthService();
 
@@ -12,6 +26,7 @@ class AuthViewModel extends ChangeNotifier {
   String get role => _role;
   String get errorMessage => _errorMessage;
 
+  // Authenticates the user with email and password.
   Future<bool> login(String email, String password) async {
     try {
       _isLoading = true;
@@ -35,6 +50,7 @@ class AuthViewModel extends ChangeNotifier {
     }
   }
 
+  // Logs out the current user and clears local authentication state
   Future<void> logout() async {
     await _authService.logout();
 
